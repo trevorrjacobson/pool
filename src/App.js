6259bcd669ac0d610afd1f781eab79a2c23825ab
trevorrjacobson <trevor.r.jacobson@gmail.com>
2021-08-03 13:05:24 -0500

    added a canvas

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Canvas } from "react-three-fiber";
+
+import Scene from "./views/Scene";
+
+function App() {
+  return (
+    <Canvas>
+      <Scene />
+    </Canvas>
+  );
+}
+
+export default App;
