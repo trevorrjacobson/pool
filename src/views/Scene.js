019f45ee33eae89869ef8263c51bd713250fecfd
trevorrjacobson <trevor.r.jacobson@gmail.com>
2021-08-03 14:23:24 -0500

    Edited lighting

--- src/views/Scene.js
+++ src/views/Scene.js
@@ -1,11 +1,46 @@
 import React from "react";
-
+import { useThree } from "react-three-fiber";
+import Lights from "../components/Lights";
 function Scene() {
+  const { camera } = useThree();
+
+  camera.fov = 45;
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.near = 0.1;
+  camera.far = 1000;
+
+  camera.up.set(0, 0, 1);
+  camera.position.set(-5, 7, 5);
+
   return (
-    <mesh>
-      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshNormalMaterial attach="material" />
-    </mesh>
+    <>
+      <mesh>
+        <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+        <meshNormalMaterial attach="material" />
+      </mesh>
+
+      <Lights
+        type="AmbientLight"
+        color={0xffffff}
+        intensity={0.2}
+        position={[0, 0, 0]}
+      />
+      {[
+        [-5, -12, 20],
+        [5, -12, 20],
+        [-5, 12, 20],
+        [5, 12, 20],
+      ].map((pos) => (
+        <Lights
+          type="PointLight"
+          color={0xffffff}
+          intensity={0.4}
+          distance={100}
+          position={pos}
+          castShadow
+        />
+      ))}
+    </>
   );
 }
 
