677e67c8d0b51effd68d52f20e0c08a1fc323560
trevorrjacobson <trevor.r.jacobson@gmail.com>
2021-08-03 13:33:08 -0500

    Controls make cube visable in 3d

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Canvas } from "react-three-fiber";
-
+import Controls from "./components/Controls";
 import Scene from "./views/Scene";
 
 function App() {
   return (
     <Canvas>
       <Scene />
+      <Controls />
     </Canvas>
   );
 }
