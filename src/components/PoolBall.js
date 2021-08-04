5cf4447d6da50103a4fbbdc74339220c920c392f
trevorrjacobson <trevor.r.jacobson@gmail.com>
2021-08-04 12:03:15 -0500

    added ball png files

---
+++ src/components/PoolBall.js
@@ -0,0 +1,37 @@
+import React, { useMemo } from "react";
+import PropTypes from "prop-types";
+import { TextureLoader, Vector2 } from "three";
+
+function PoolBall({ setRef, position, textureURL }) {
+  const ballTexture = useMemo(
+    () => new TextureLoader().load(textureURL),
+    [textureURL]
+  );
+
+  return (
+    <mesh ref={setRef} position={position} speed={new Vector2()} castShadow>
+      <sphereGeometry attach="geometry" args={[0.5, 128, 128]} />
+      <meshStandardMaterial
+        attach="material"
+        color={0xffffff}
+        roughness={0.25}
+        metalness={0}
+        map={ballTexture}
+      />
+    </mesh>
+  );
+}
+
+PoolBall.propTypes = {
+  setRef: PropTypes.objectOf(PropTypes.any),
+  position: PropTypes.arrayOf(PropTypes.number),
+  textureURL: PropTypes.string,
+};
+
+PoolBall.defaultProps = {
+  setRef: {},
+  position: [],
+  textureURL: "",
+};
+
+export default PoolBall;
--- src/views/Scene.js
+++ src/views/Scene.js
@@ -2,6 +2,24 @@ import React from "react";
 import { useThree } from "react-three-fiber";
 import Lights from "../components/Lights";
 import PoolTable from "../components/PoolTable";
+import PoolBall from "../components/PoolBall";
+import cueBall from "../assets/cueball.png";
+import one from "../assets/1.png";
+import two from "../assets/2.png";
+import three from "../assets/3.png";
+import four from "../assets/4.png";
+import five from "../assets/5.png";
+import six from "../assets/6.png";
+import seven from "../assets/7.png";
+import eight from "../assets/8.png";
+import nine from "../assets/9.png";
+import ten from "../assets/10.png";
+import eleven from "../assets/11.png";
+import twelve from "../assets/12.png";
+import thirteen from "../assets/13.png";
+import fourteen from "../assets/14.png";
+import fifteen from "../assets/15.png";
+
 function Scene() {
   const { camera } = useThree();
 
@@ -39,6 +57,24 @@ function Scene() {
       <React.Suspense fallback={<mesh />}>
         <PoolTable />
       </React.Suspense>
+      <object3D>
+        <PoolBall position={[0, -16, 0]} textureURL={cueBall} />
+        <PoolBall position={[-1.01, 15, 0]} textureURL={one} />
+        <PoolBall position={[1.01, 17, 0]} textureURL={two} />
+        <PoolBall position={[-0.51, 16, 0]} textureURL={three} />
+        <PoolBall position={[-1.01, 17, 0]} textureURL={four} />
+        <PoolBall position={[-2.02, 17, 0]} textureURL={five} />
+        <PoolBall position={[1.53, 16, 0]} textureURL={six} />
+        <PoolBall position={[0.51, 14, 0]} textureURL={seven} />
+        <PoolBall position={[0, 15, 0]} textureURL={eight} />
+        <PoolBall position={[0, 13, 0]} textureURL={nine} />
+        <PoolBall position={[0.51, 16, 0]} textureURL={ten} />
+        <PoolBall position={[2.02, 17, 0]} textureURL={eleven} />
+        <PoolBall position={[-0.51, 14, 0]} textureURL={twelve} />
+        <PoolBall position={[0, 17, 0]} textureURL={thirteen} />
+        <PoolBall position={[-1.53, 16, 0]} textureURL={fourteen} />
+        <PoolBall position={[1.01, 15, 0]} textureURL={fifteen} />
+      </object3D>
     </>
   );
 }
